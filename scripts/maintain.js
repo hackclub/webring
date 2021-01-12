977727320991be1a363cec18b71402e87798ddba
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-12 09:22:13 +0530

    Add webring maintenance scripts and GitHub action (#44)
    * Add gitignore
    
    * Add scripts
    
    * Add maintain.js
    
    * Add maintenance github action
    
    * Maintain Webring
    
    * Allow git commit to fail if empty
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ scripts/maintain.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch')
+const { readFileSync, writeFileSync } = require('fs')
+
+const members = JSON.parse(readFileSync('../members.json', { encoding: "utf8" }))
+let activeMembers = []
+
+const webringLink = "https://webring.hackclub.com/"
+
+async function main () {
+    for (const approvedMember of members) {
+        try {
+            const response = await fetch(approvedMember.url)
+            if (!response.ok) throw "Unreachable page"
+
+            const data = await response.text()
+            const strippedHTML = data.replace(/\s/g, '')
+
+            if (strippedHTML.includes(webringLink)) {
+                activeMembers.push(approvedMember)
+            } else {
+                throw "Page doesn't contain webring code"
+            }
+        } catch (e) {
+            console.log(`${approvedMember.member} was rejected. Reason:`)
+            console.log(e)
+        }
+    }
+
+    writeFileSync('../public/members.json', JSON.stringify(activeMembers))
+}
+
+main()
