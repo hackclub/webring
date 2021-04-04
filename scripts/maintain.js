a1ad79b2c3d0dd7c050acacef355212bcce117ea
Steven Conaway <steven.conaway@icloud.com>
2021-04-04 13:42:06 -0700

    maintain: allow content check to be bypassed and add some debugging

--- scripts/maintain.js
+++ scripts/maintain.js
@@ -13,6 +13,7 @@ const webringLink = "https://webring.hackclub.com/"
 async function checkWithBrowser(url) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+
     await page.goto(url)
 
     let containsLink = false
@@ -27,9 +28,18 @@ async function checkWithBrowser(url) {
 
 async function main() {
     for (const approvedMember of members) {
+        console.debug(`Trying ${approvedMember.member}`)
         try {
             const response = await fetch(approvedMember.url)
             if (!response.ok) throw "Unreachable page"
+
+            if (approvedMember.bypass) {
+                console.debug(
+                    `${approvedMember.member}'s page is marked with \`"bypass": true\``
+                )
+                activeMembers.push(approvedMember)
+                continue
+            }
 
             const data = await response.text()
             const strippedHTML = data.replace(/\s/g, "")
@@ -38,7 +48,7 @@ async function main() {
                 activeMembers.push(approvedMember)
             } else {
                 // some sites might use plain react, which means we need to
-                // actually open the page in the browser and then search the
+                // actually open the page in a browser and then search the
                 // rendered HTML from the browser
                 console.debug(
                     `${approvedMember.member}'s page didn't contain the code in the HTML, but let's render it in the browser to be sure it's not there.`
